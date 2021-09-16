4701533d12f3d6843f57226d5ecc95ac062f4488
vaagnmazmanyan <vmazmanyanvaagn@gmail.com>
2021-09-16 16:09:59 +0400

    Fix copy buttons and put loader in tokens page.

--- packages/page-nft-wallet/src/containers/NetworkWallet/index.tsx
+++ packages/page-nft-wallet/src/containers/NetworkWallet/index.tsx
@@ -8,11 +8,13 @@ import type { NftCollectionInterface } from '@polkadot/react-hooks/useCollection
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk';
 import React, { useCallback, useContext } from 'react';
 import Button from 'semantic-ui-react/dist/commonjs/elements/Button/Button';
+import Loader from 'semantic-ui-react/dist/commonjs/elements/Loader';
 
 import GetTestUNQModal from '@polkadot/app-nft-wallet/components/GetTestUNQModal';
 import { OpenPanelType } from '@polkadot/apps-routing/types';
 import { ChainImg, CopyIcon } from '@polkadot/react-components';
 import StatusContext from '@polkadot/react-components/Status/Context';
+import { useTranslation } from '@polkadot/react-components/translate';
 import { useBalances, useNetworkInfo, useToggle } from '@polkadot/react-hooks';
 import { formatKsmBalance } from '@polkadot/react-hooks/useKusamaApi';
 import { FormatBalance } from '@polkadot/react-query';
@@ -41,14 +43,20 @@ function NetworkWallet ({ account }: NftWalletProps): React.ReactElement {
   const { queueAction } = useContext(StatusContext);
   const [major, rest] = formatBalance(fullBalance?.availableBalance);
 
-  const _onCopy = useCallback(
-    (address: string) => queueAction({
-      account: address,
-      action: 'clipboard',
-      message: 'address copied',
-      status: 'queued'
-    }),
-    [queueAction]
+  const { t } = useTranslation();
+
+  const copyAddress = useCallback(
+    (account: string) => {
+      void navigator.clipboard.writeText(account);
+
+      return queueAction({
+        account,
+        action: t('clipboard'),
+        message: t('address copied'),
+        status: 'queued'
+      });
+    },
+    [queueAction, t]
   );
 
   const handleGetKSMClickByRamp = () => {
@@ -69,102 +77,109 @@ function NetworkWallet ({ account }: NftWalletProps): React.ReactElement {
           Network tokens
         </div>
         <div className='network-wallet--block--body'>
-          { kusamaChain && (
-            <div className='token-item'>
-              <div className='token-item--account'>
-                <span>
-                  <ChainImg
-                    className='endpointIcon'
-                    isInline
-                    logo={kusamaChain.toLowerCase()}
-                    withoutHl
-                  />
-                  {kusamaChain}
-                </span>
-                <span>
-                  {encodedKusamaAccount}
-                  <a onClick={account ? _onCopy.bind(null, account) : () => null }>
-                    <CopyIcon color={'var(--input-placeholder-search-color)'} />
-                  </a>
-                </span>
+          {kusamaChain
+            ? (<>
+              <div className='token-item'>
+                <div className='token-item--account'>
+                  <span>
+                    <ChainImg
+                      className='endpointIcon'
+                      isInline
+                      logo={kusamaChain.toLowerCase()}
+                      withoutHl
+                    />
+                    {kusamaChain}
+                  </span>
+                  <span>
+                    {encodedKusamaAccount}
+                    <a onClick={encodedKusamaAccount ? copyAddress.bind(null, encodedKusamaAccount) : () => null }>
+                      <CopyIcon color={'var(--input-placeholder-search-color)'} />
+                    </a>
+                  </span>
+                </div>
+                <div className='token-item--balances'>
+                  <span className='token-item--balances--row'>
+                    {formatKsmBalance(fullKusamaBalance?.freeBalance)} KSM
+                  </span>
+                  <span className='token-item--balances--row'>
+                    <span>{formatKsmBalance(fullKusamaBalance?.availableBalance)} KSM transferrable</span>
+                    <span>{formatKsmBalance(fullKusamaBalance?.lockedBalance)} KSM locked</span>
+                  </span>
+                </div>
+                <div className='token-item--actions'>
+                  <Button
+                    disabled={!+formatKsmBalance(fullKusamaBalance?.availableBalance)}
+                    onClick={toggleTransfer}
+                  >
+                    Send
+                  </Button>
+                  <Button
+                    onClick={handleGetKSMClickByRamp}
+                  >
+                     Get
+                  </Button>
+                </div>
               </div>
-              <div className='token-item--balances'>
-                <span className='token-item--balances--row'>
-                  {formatKsmBalance(fullKusamaBalance?.freeBalance)} KSM
-                </span>
-                <span className='token-item--balances--row'>
-                  <span>{formatKsmBalance(fullKusamaBalance?.availableBalance)} KSM transferrable</span>
-                  <span>{formatKsmBalance(fullKusamaBalance?.lockedBalance)} KSM locked</span>
-                </span>
-              </div>
-              <div className='token-item--actions'>
-                <Button
-                  disabled={!+formatKsmBalance(fullKusamaBalance?.availableBalance)}
-                  onClick={toggleTransfer}
-                >
+              <div className='token-item'>
+                <div className='token-item--account'>
+                  <span>
+                    <ChainImg
+                      className='endpointIcon'
+                      isInline
+                      withoutHl
+                    />
+                    {chain}
+                  </span>
+                  <span>
+                    {account}
+                    <a onClick={account ? copyAddress.bind(null, account) : () => null }>
+                      <CopyIcon color={'var(--input-placeholder-search-color)'} />
+                    </a>
+                  </span>
+                </div>
+                <div className='token-item--balances'>
+                  <span className='token-item--balances--row'>
+                    <FormatBalance
+                      className='result'
+                      value={fullBalance?.freeBalance}
+                    />
+                  </span>
+                  <span className='token-item--balances--row'>
+                    <span>
+                      <FormatBalance
+                        className='result'
+                        value={fullBalance?.availableBalance}
+                      /> transferable
+                    </span>
+                    <span>
+                      <FormatBalance
+                        className='result'
+                        value={fullBalance?.lockedBalance}
+                      /> locked
+                    </span>
+                  </span>
+                </div>
+                <div className='token-item--actions'>
+                  <Button
+                    disabled={!+major && !+rest}
+                    onClick={toggleTransfer}
+                  >
                   Send
-                </Button>
-                <Button
-                  onClick={handleGetKSMClickByRamp}
-                >
+                  </Button>
+                  <Button
+                    onClick={toggleGetTestUNQModal}
+                  >
                   Get
-                </Button>
+                  </Button>
+                </div>
               </div>
-            </div>
-          )}
-          <div className='token-item'>
-            <div className='token-item--account'>
-              <span>
-                <ChainImg
-                  className='endpointIcon'
-                  isInline
-                  withoutHl
-                />
-                {chain}
-              </span>
-              <span>
-                {account}
-                <a onClick={account ? _onCopy.bind(null, account) : () => null }>
-                  <CopyIcon color={'var(--input-placeholder-search-color)'} />
-                </a>
-              </span>
-            </div>
-            <div className='token-item--balances'>
-              <span className='token-item--balances--row'>
-                <FormatBalance
-                  className='result'
-                  value={fullBalance?.freeBalance}
-                />
-              </span>
-              <span className='token-item--balances--row'>
-                <span>
-                  <FormatBalance
-                    className='result'
-                    value={fullBalance?.availableBalance}
-                  /> transferable
-                </span>
-                <span>
-                  <FormatBalance
-                    className='result'
-                    value={fullBalance?.lockedBalance}
-                  /> locked
-                </span>
-              </span>
-            </div>
-            <div className='token-item--actions'>
-              <Button
-                disabled={!+major && !+rest}
-                onClick={toggleTransfer}
-              >
-                Send
-              </Button>
-              <Button
-                onClick={toggleGetTestUNQModal}
-              >
-                Get
-              </Button>
-            </div>
-          </div>
+            </>)
+            : <Loader
+              active
+              inline='centered'
+              key={'nft-wallet'}
+              size='large'
+            />}
         </div>
       </div>
       { isTransferOpen && (
