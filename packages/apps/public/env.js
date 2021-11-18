334a9105321f395a1f6a37ea33be645ddf115934
Nikolay <2854241+pasynkov@users.noreply.github.com>
2021-11-18 16:11:18 +0400

    Update env.js

--- packages/apps/public/env.js
+++ packages/apps/public/env.js
@@ -42,7 +42,7 @@
     KUSAMA_API: defaults('${KUSAMA_API}', 'wss://kusama-rpc.polkadot.io'),
     KUSAMA_BACKUP_API: defaults('${KUSAMA_BACKUP_API}', 'wss://polkadot.api.onfinality.io/public-ws'),
     GRAPH_QL_API: defaults('${GRAPH_QL_API}', 'https://dev-api-explorer.unique.network/v1/graphql'),
-    GRAPH_QL_ADMIN_SECRET: defaults('${GRAPH_QL_ADMIN_SECRET}', 'hepM3wfsATBoI-ix2uhsAodr1j99MThPF5LBZJI2YtHAax7W9BIP9F8IWuzcNUC4')
+    GRAPH_QL_ADMIN_SECRET: defaults('${GRAPH_QL_ADMIN_SECRET}', '')
   };
 
   // eslint-disable-next-line no-template-curly-in-string
