ea52028bc2f498e824258d6adbd1ff58dfcab10e
pasynkov <n@pasynkov.com>
2022-01-21 14:17:08 +0700

    add dotenv

--- packages/apps-config/src/envConfig.ts
+++ packages/apps-config/src/envConfig.ts
@@ -23,7 +23,7 @@ export type EnvConfigType = {
 
 declare global {
   interface Window {
-    ENV: {
+    ENV?: {
       ENVIRONMENT: string;
       FAVICON_PATH: string;
       GRAPH_QL_ADMIN_SECRET: string;
@@ -46,23 +46,23 @@ declare global {
 }
 
 const envConfig: EnvConfigType = {
-  environment: window.ENV.ENVIRONMENT,
-  faviconPath: window.ENV.FAVICON_PATH,
-  graphQlAdminSecret: window.ENV.GRAPH_QL_ADMIN_SECRET,
-  graphQlApi: window.ENV.GRAPH_QL_API,
-  ipfsGateway: window.ENV.IPFS_GATEWAY,
-  kusamaApiUrl: window.ENV.KUSAMA_API,
-  kusamaBackupApiUrl: window.ENV.KUSAMA_BACKUP_API,
-  kusamaDecimals: +window.ENV.KUSAMA_DECIMALS,
-  maxGas: +window.ENV.MAX_GAS,
-  minPrice: +window.ENV.MIN_PRICE,
-  quoteId: +window.ENV.QUOTE_ID,
-  uniqueCollectionIds: window.ENV.UNIQUE_COLLECTION_IDS.split(','),
-  uniqueSubstrateApi: window.ENV.UNIQUE_SUBSTRATE_API,
-  uniqueTelegram: window.ENV.UNIQUE_TELEGRAM,
-  value: +window.ENV.VALUE,
-  version: window.ENV.VERSION,
-  whiteLabelUrl: window.ENV.WHITE_LABEL_URL
+  environment: window.ENV?.ENVIRONMENT || process.env.ENVIRONMENT,
+  faviconPath: window.ENV?.FAVICON_PATH || process.env.FAVICON_PATH,
+  graphQlAdminSecret: window.ENV?.GRAPH_QL_ADMIN_SECRET || process.env.GRAPH_QL_ADMIN_SECRET,
+  graphQlApi: window.ENV?.GRAPH_QL_API || process.env.GRAPH_QL_API,
+  ipfsGateway: window.ENV?.IPFS_GATEWAY || process.env.IPFS_GATEWAY,
+  kusamaApiUrl: window.ENV?.KUSAMA_API || process.env.KUSAMA_API,
+  kusamaBackupApiUrl: window.ENV?.KUSAMA_BACKUP_API || process.env.KUSAMA_BACKUP_API,
+  kusamaDecimals: +(window.ENV?.KUSAMA_DECIMALS || process.env.KUSAMA_DECIMALS),
+  maxGas: +(window.ENV?.MAX_GAS || process.env.MAX_GAS),
+  minPrice: +(window.ENV?.MIN_PRICE || process.env.MIN_PRICE),
+  quoteId: +(window.ENV?.QUOTE_ID || process.env.QUOTE_ID),
+  uniqueCollectionIds: (window.ENV?.UNIQUE_COLLECTION_IDS || process.env.UNIQUE_COLLECTION_IDS).split(','),
+  uniqueSubstrateApi: window.ENV?.UNIQUE_SUBSTRATE_API || process.env.UNIQUE_SUBSTRATE_API,
+  uniqueTelegram: window.ENV?.UNIQUE_TELEGRAM || process.env.UNIQUE_TELEGRAM,
+  value: Number(window.ENV?.VALUE || process.env.VALUE),
+  version: window.ENV?.VERSION || process.env.VERSION,
+  whiteLabelUrl: window.ENV?.WHITE_LABEL_URL || process.env.WHITE_LABEL_URL
 };
 
 export default envConfig;
